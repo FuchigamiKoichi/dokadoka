0e76f4e98bf43ce24ed537e04356aebf4086a1ba
Nishime Kenta <e235703@ie.u-ryukyu.ac.jp>
2023-12-16 04:13:18 +0900

    waknneeeeeyo

--- app/src/main/java/jp/ac/uryukyu/ie/e235703_e235714_e235726_e235753_e235758/Calculation/Main.java
+++
@@ -1,9 +0,0 @@
-package jp.ac.uryukyu.ie.e235703_e235714_e235726_e235753_e235758.Calculation;
-
-public class Main {
-    public static void main(String[] args) {
-        Character player1 = new Character();
-        //テスト
-    }
-    
-}
