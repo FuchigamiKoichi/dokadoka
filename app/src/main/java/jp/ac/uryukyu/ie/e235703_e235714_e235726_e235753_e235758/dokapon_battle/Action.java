61b21130ea3f30306a3b8200e55db5e911b5f4b0
Tamashiro Shigeharu <e235726@ie.u-ryukyu.ac.jp>
2023-12-15 15:43:07 +0900

    first

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235703_e235714_e235726_e235753_e235758/dokapon_battle/Action.java
@@ -0,0 +1,5 @@
+package jp.ac.uryukyu.ie.e235703_e235714_e235726_e235753_e235758.dokapon_battle;
+public interface Action {
+    String getName();
+    void execute(FightCharacter executer,FightCharacter target);
+}
